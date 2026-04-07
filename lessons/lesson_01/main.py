dda9b8d01446115ebcc9f3c87cf3be6758ca22e3
NagyIstvanGabor <nagy.istvan.gabor@protonmail.com>
2026-04-07 15:29:23 +0200

    initial commit

---
+++ lessons/lesson_01/main.py
@@ -0,0 +1,60 @@
+print("Hello World!")
+
+#SINGLE LINE KOMMENT / EGY SOROS KOMMENT
+
+"""
+multi
+line
+comment
+"""
+
+'''
+multi
+line
+comment
+'''
+
+#PRIMITIVE DATA TYPES / PRIMITÍV ADATTÍPUSOK
+
+#STRING TYPE / KARAKTERLÁNC TÍPUS
+print("apple")
+print("Jimmy likes apple")
+
+print('apple')
+print('123')
+print('True')
+#INTEGER TYPE / EGÉSZ SZÁM TÍPUS
+
+print(123)
+print(12)
+
+#FLOAT TYPE / LEBEGŐPONTOS SZÁM TÍPUS
+
+print(3.14)
+print(3.155534)
+
+#BOOLEAN TÍPUS / LOGIKAI TÍPUS
+
+print(True)
+print(False)
+
+#NONE TYPE / INFORMÁCIÓ HIÁNYA
+
+print(None)
+
+
+#TERMINAL COMMANDS USED DURING THE LESSON:
+"""
+python --version
+cd 
+dir
+mkdir
+
+python main.py
+python -m venv env
+env\scripts\activate
+pip install fastapi
+pip freeze > requirements.txt
+pip install -r requiremens.txt
+pip uninstall fastapi 
+"""
---
+++ lessons/lesson_01/virtual_env_practice.py
@@ -0,0 +1,8 @@
+#STANDARD LIBRARY
+import random
+#PYPI
+import fastapi
+
+print(random.randint(1,10))
+
+app = fastapi.FastAPI()
