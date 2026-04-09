e56ce853f13f14acc7bc70ce2efd0bf3da9bd0aa
Rakos Gegrely <Gergely.Rakos.ext@dwpbank.de>
2026-04-09 19:05:16 +0200

    My first HW in the Python course

---
+++ homeworks/rakosgergelypeter/hw_01_introduction/main.py
@@ -0,0 +1,34 @@
+"""
+This is my first Python
+program
+"""
+ #int
+print('This is an int!')
+number=5
+print(number)
+print(type(number))
+print('---------------')
+#float
+print('This is a float!')
+PI=3.14
+print(PI)
+print(type(PI))
+print('---------------')
+#string
+print('This is a string!')
+name="Geri"
+print(name)
+print(type(name))
+print('---------------')
+#boolean
+print('This is a boolean!')
+is_it_a_dog=True
+print(is_it_a_dog)
+print(type(is_it_a_dog))
+print('---------------')
+#none
+print('This is a None!')
+nothing=None
+print(nothing)
+print(type(nothing))
+print('---------------')
