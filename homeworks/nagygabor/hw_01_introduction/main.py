e2f2a745fdf3a38364b713e6ae9eac4f4c42674b
NGabi86 <gabor.nagy1886@gmail.com>
2026-04-11 17:51:25 +0200

    Update main.py

---
+++ homeworks/nagygabor/hw_01_introduction/main.py
@@ -0,0 +1,28 @@
+'''
+This is my first Python program /
+Es ist mein erstes Pytnon program /
+Detta är mitt första Python program /
+Ez az első python Programom
+'''
+#STRING
+
+print ('apple') 
+
+#BOOLEAN
+
+print(True)  
+print(False)
+
+#NONE
+
+print (None)
+
+#INTEGRATE
+
+print (123)
+print(12)
+
+#FLOAT
+
+print(3,14568)
+
