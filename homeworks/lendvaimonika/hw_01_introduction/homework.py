42066ca5f0dd8514c2e31fe54faaa33455c21526
llemo4455 <lendvaimo@icloud.com>
2026-04-12 17:15:36 +0200

    homework v2 without space

--- homeworks/lendvaimonika/hw_01_introduction/homework.py
+++ homeworks/lendvaimonika/hw_01_introduction/homework.py
@@ -2,8 +2,8 @@
 This is my first Python program
 """
 
-print ('Hello World') # string type - szöveges vagy karakterlánc
-print (123) #integer type - egész szám
-print (3.14) #float type - tört vagy lebegőpontos szám
-print (True) #boolean type - logikai
-print (None) #none type - információ hiánya
+print('Hello World') # string type - szöveges vagy karakterlánc
+print(123) #integer type - egész szám
+print(3.14) #float type - tört vagy lebegőpontos szám
+print(True) #boolean type - logikai
+print(None) #none type - információ hiánya
