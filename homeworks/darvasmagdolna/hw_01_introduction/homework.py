a0b11b2a8b8e31e19121247d950c0687502c1d69
Magdolna99 <kisribizli@yahoo.com>
2026-04-12 13:20:26 +0200

    homework_1_ ready

---
+++ homeworks/darvasmagdolna/hw_01_introduction/homework.py
@@ -0,0 +1,9 @@
+"""
+This is my first Python program.
+"""
+print (Izgalmas) # string type/karakterlánc
+print (1,2,3) # integer type/egész szám
+print (2.5) # float type/lebegő pontos szám
+print (true/false) # boolean type/logikai érték
+print (none) # none type/információ hiány
+
