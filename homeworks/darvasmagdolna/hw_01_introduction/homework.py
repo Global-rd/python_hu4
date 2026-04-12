7aa568b3d5a1436f239abe50e75c4340d02c252e
Magdolna99 <kisribizli@yahoo.com>
2026-04-12 15:54:22 +0200

    homework_1_ ready 2
    Ez a végleges hw_1

--- homeworks/darvasmagdolna/hw_01_introduction/homework.py
+++ homeworks/darvasmagdolna/hw_01_introduction/homework.py
@@ -1,10 +1,10 @@
 """
 This is my first Python program.
 """
-print (Izgalmas) # string type/karakterlánc
-print (1,2,3) # integer type/egész szám
-print (1,2,3,4,5,6,7,8,9,10) # integer long type/annyi szám, amennyi belefér
-print (2.5) # float type/lebegő pontos szám
-print (true/false) # boolean type/logikai érték
-print (none) # none type/információ hiány
+print ("Izgalmas") # string (str) type/karakterlánc
+print ("1,2,3") # integer (int) type/egész szám
+print ("1000000000000") # integer long type/annyi szám, amennyi belefér
+print ("2.5") # float type/lebegő pontos szám
+print ("True/False") # boolean (bool) type/logikai érték
+print ("none") # none type/információ hiány
 
