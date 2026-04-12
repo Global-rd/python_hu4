c2e633b407a1677f37e24ea90e8eb13e797b8638
Magdolna99 <kisribizli@yahoo.com>
2026-04-12 13:43:14 +0200

    correcting issue in darvasmagdolna/homework-1

--- homeworks/darvasmagdolna/hw_01_introduction/homework.py
+++ homeworks/darvasmagdolna/hw_01_introduction/homework.py
@@ -3,6 +3,7 @@ This is my first Python program.
 """
 print (Izgalmas) # string type/karakterlánc
 print (1,2,3) # integer type/egész szám
+print (1,2,3,4,5,6,7,8,9,10) # integer long type/annyi szám, amennyi belefér
 print (2.5) # float type/lebegő pontos szám
 print (true/false) # boolean type/logikai érték
 print (none) # none type/információ hiány
