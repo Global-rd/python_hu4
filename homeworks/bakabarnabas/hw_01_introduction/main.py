1eb22fb3ec93d16f69eb8929cc16a0286ff977ad
bakabarnabas <bakabarnabas9@gmail.com>
2026-04-13 10:32:02 +0200

    Create main.py

---
+++ homeworks/bakabarnabas/hw_01_introduction/main.py
@@ -0,0 +1,15 @@
+"""
+This is my 
+first
+Python program 
+"""
+
+print("Molly") # String type
+
+print(25) # Integer type
+
+print(3.14) # Float type
+
+print(True) # Boolean type
+
+print(None) # None type
