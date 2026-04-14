b97a93267bb759ade5600e48a22a080d16b02190
psymon-peter <psymon.peter@gmail.com>
2026-04-14 23:56:51 +0200

    Create hw01.py

---
+++ homeworks/simonpeter/hw_01_introduction/hw01.py
@@ -0,0 +1,13 @@
+"""
+
+This is my first Python program
+
+"""
+
+#Primitív adattípusok
+print(150) #integer: egész szám
+print(45.138) #float: lebegőpontos szám 
+print("asdf123") #string: szöveg/ karakterlánc 
+print(True) #boolean: logikai típus - Igaz érték 
+print(False) #booelan: logikai típus - Hamis érték
+print(None) #None: információ hiánya
