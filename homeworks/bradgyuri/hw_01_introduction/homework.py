6f230aa27884c1a1a8c8b085850073cb956d3d02
Gyurrri <gybrad@gmail.com>
2026-04-14 23:12:08 +0200

    Update homework.py

--- homeworks/bradgyuri/hw_01_introduction/homework.py
+++ homeworks/bradgyuri/hw_01_introduction/homework.py
@@ -7,4 +7,4 @@ print('Patrik') #karakter típus
 print(1981)     #egész szám típus
 print(18.65)     #lebegőpontos szám típus   
 print(True)     #logikai típus
-print (None)     #információ hiánya típus
+print(None)     #információ hiánya típus
