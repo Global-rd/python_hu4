51aa7f353fad2022f04087ceda81477fc1f6c34c
geggenet <geggezor@gmail.com>
2026-04-17 14:16:18 +0200

    Create homework.py

---
+++ homeworks/pocsaigergely/hw_01_introduction/homework.py
@@ -0,0 +1,11 @@
+"""
+This is my first
+Python program
+"""
+
+print('Hello Python') # string
+print(123) #integer
+print(3.14) #float
+print(True) #boolean
+print(None) #none vagyis nincs semmi értelmezve
+
