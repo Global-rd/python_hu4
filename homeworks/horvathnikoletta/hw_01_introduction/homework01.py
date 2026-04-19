2a7558e1e0e944cd7ca0d68603dc0bdbce445d39
HNiky90 <89686866+HNiky90@users.noreply.github.com>
2026-04-19 21:23:26 +0200

    homework-1 ready

---
+++ homeworks/horvathnikoletta/hw_01_introduction/homework01.py
@@ -0,0 +1,16 @@
+"""This 
+is
+my 
+first
+Python
+program"""
+
+print("kutya") #String type/Karakterlánc típus
+
+print(77) #Integer type/Egész szám típus
+
+print(3.21) #Float type/Nem egész szám típus
+
+print(True) #Boolean type/Logikai típus
+
+print(None) #Non type/Információ hiánya
