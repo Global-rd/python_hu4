06115439c3808306d21f9dbd8a6638014ee20806
csitepeter-hue <csitepeter@gmail.com>
2026-04-21 22:05:15 +0200

    adding to the homework.py

---
+++ homeworks/csitepeter/hw_01_introduction/homework.py
@@ -0,0 +1,5 @@
+"""This is my first Python program"""
+print("Bananas") # String
+print("5.8457")  # Lebegőpontos szám
+print("True")    # Boolean-logikai érték
+print("None")    # Információ hiánya
