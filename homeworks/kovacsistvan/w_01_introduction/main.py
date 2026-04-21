5f2144dc6785e5d05cff82db95bb06ecc0c63711
busistvankovacs <bus.istvan.kovacs@gmail.com>
2026-04-21 21:50:37 +0200

    bugfix

--- homeworks/kovacsistvan/w_01_introduction/main.py
+++ homeworks/kovacsistvan/w_01_introduction/main.py
@@ -7,16 +7,16 @@ program"""
 összes primitív adattípusból amit tanultunk. Írd mindegyik mellé kommentben
 a típusát is"""""
 
-string = "im a string" #string type
-integer = 1 #integer type
-float = 1.11 #float type
-booleant = True #boolean type 
-booleanf = False #boolean type 
-none = None #none type
+my_string = "im a string" #string type
+my_integer = 1 #integer type
+my_float = 1.11 #float type
+my_booleant = True #boolean type 
+my_booleanf = False #boolean type 
+my_none = None #none type
 
-print(string)
-print(integer)
-print(float)
-print(booleant)
-print(booleanf)
-print(none)
+print(my_string)
+print(my_integer)
+print(my_float)
+print(my_booleant)
+print(my_booleanf)
+print(my_none)
