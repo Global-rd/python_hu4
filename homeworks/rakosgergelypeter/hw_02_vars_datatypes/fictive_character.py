615d61649707e83edcdd506e32515ab1d5c47d7a
rakosgergelypeter <rakosgergelypeter@gmail.com>
2026-04-23 11:10:14 +0200

    rakosgergelypeter/homewrok_2 correction

--- homeworks/rakosgergelypeter/hw_02_vars_datatypes/fictive_character.py
+++ homeworks/rakosgergelypeter/hw_02_vars_datatypes/fictive_character.py
@@ -10,11 +10,12 @@ while  name!=name.upper().strip():
 #while the age is not in good conversion
 age=0
 while True:
+    user_input=input("Kérlek add meg az életkorod:")
     try:
-        age=int(input("Kérlek add meg az életkorod:"))
+        age=int(user_input)
         break
     except ValueError:
-        print(f"nem jó a szám {age}")
+        print(f"nem jó a szám {user_input}")
         continue
 #konverted date        
 days=age*365
@@ -23,13 +24,15 @@ python_experinece=0
 while True:
     try:
         python_experinece=int(input("Kérlek add meg hány év Python tapasztalatod van:"))
+        if python_experinece < 0:
+            print("Nem lehet negatív szám!")
+            continue
         break
     except ValueError:
         print(f"nem jó a szám {python_experinece}")
 
 #want to be a professional
-is_professional=True if input("Szeretnél profi lenni:").upper()=="YES" else False
+dev_intention = "wants" if input("Szeretnél profi lenni:").upper()== "YES" else "does not want"
 
 print(f"My character {age} (years) in {days} (days) old. His/her name is {name} and he/she has {python_experinece} years experience. ")
-print("He/she wants to be a Python developer!" if is_professional else "He/she  dont wants to be a Python developer!")
-
+print(f"He/she {dev_intention} to be a Python developer!")
