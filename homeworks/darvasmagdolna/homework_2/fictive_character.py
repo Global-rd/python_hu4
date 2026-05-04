f29acddd2daf6022922dfd0b5d0c6d339334d20f
Magdolna99 <kisribizli@yahoo.com>
2026-05-04 20:03:50 +0200

    Homework2 - javitas

--- homeworks/darvasmagdolna/homework_2/fictive_character.py
+++ homeworks/darvasmagdolna/homework_2/fictive_character.py
@@ -1,13 +1,7 @@
 
-<<<<<<< Updated upstream
 name = input("Hogy hívnak?" )
 age = int(input("Hány éves vagy? "))
 python_exp_in_years = int(input("Hány év Python gyakorlatod van? "))
-=======
-name = input("Hogy hívnak? ")
-age = int(input("Hány éves vagy? "))
-python_exp_in_years = int(input("Hány év Python tapasztalatod van? "))
->>>>>>> Stashed changes
 
 print(name)
 print(age)
@@ -23,17 +17,7 @@ print(f"My character is {age_in_days} old. His/her name is {name} and he/she has
 
 # extra task
 
-<<<<<<< Updated upstream
 question = input("Szeretnél-e profi python fejlesztő lenni?")
 dev_intention = "wants" if question== "yes" else "does not want"
    
 print(f"My character is {age_in_days} old. His/her name is {name} and he/she has {python_exp_in_years} years experience. He/she {"wants" if question== "yes" else "does not want"} to be a Python developer!")
-=======
-question = input("Szeretnél-e profi python fejlesztő lenni? (yes/no) ")
-
-# változó = érték_ha_igaz if feltétel else érték_ha_hamis
-dev_intention = "wants" if question == "yes" else "does not want"
-
-print(f"My character is {age_in_days} old. His/her name is {name} and he/she has {python_exp_in_years} years experience. He/she {dev_intention} to be a Python developer!")
-print(f"My character is {age_in_days} old. His/her name is {name} and he/she has {python_exp_in_years} years experience. He/she {'wants' if question == 'yes' else 'does not want'} to be a Python developer!")
->>>>>>> Stashed changes
