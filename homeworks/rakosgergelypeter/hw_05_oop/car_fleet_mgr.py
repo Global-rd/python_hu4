4944d87f3c4d9809b351a7ab63529b970f29f3f5
rakosgergelypeter <rakosgergelypeter@gmail.com>
2026-05-12 18:25:57 +0200

    rakosgergelypeter/homework-5 2. commit

--- homeworks/rakosgergelypeter/hw_05_oop/car_fleet_mgr.py
+++ homeworks/rakosgergelypeter/hw_05_oop/car_fleet_mgr.py
@@ -1,34 +1,39 @@
 import datetime
 
-class FuelExcpetion(Exception):
+class FuelException(Exception):
     pass
 
 class Car:
-    def __init__(self,brand:str,model:str,year:datetime,milage:float=0,fuel_level:int=100):
+    def __init__(self,brand:str,model:str,year:datetime,mileage:float=0,fuel_level:int=100):
         self.brand=brand
         self.model=model
         self.year=year
-        self.milage=0
-        self.fuel_level=100
+        self.mileage=mileage
+        self.fuel_level=fuel_level
 
     def __str__(self):
-        return f"{self.brand} - {self.model} - {self.year} - {self.milage} - {self.fuel_level}"
+        return f"{self.brand} - {self.model} - {self.year} - {self.mileage} - {self.fuel_level}"
 
     def drive(self,distance:int):
         if distance!=0:
             if (self.fuel_level>(distance*0.1)):
-                self.milage+=distance
+                self.mileage+=distance
                 self.fuel_level-=(distance*0.1)
-                return f"Az autó ennyi km-et tett meg{self.milage} és ennyi a jeléenlegi üzemanyag {self.fuel_level}"
+                return f"Az autó ennyi km-et tett meg{self.mileage} és ennyi a jeléenlegi üzemanyag {self.fuel_level}"
             else:   
-                raise FuelExcpetion("Nincs elég üzemeanyagod!")
+                raise FuelException("Nincs elég üzemeanyagod!")
         else:
-            raise ZeroDivisionError()
+            raise ValueError()
         
     def refuel(self,liter:int):
             if liter <= 0:
                 raise FuelExcpetion("A tankolás mennyisége pozitív kell legyen.")
-            self.fuel_level = min(self.fuel_level + liter, self.fuel_level)
+            
+            current_fuel_l = self.fuel_level + liter
+            rest=current_fuel_l-100
+            if rest>0:
+                current_fuel_l-=rest
+            self.fuel_level=current_fuel_l
             return f"Jelenlegi üzemanyagszint: {self.fuel_level}"
         
 class Fleet:
@@ -36,17 +41,16 @@ class Fleet:
         self.cars:list[Car]=[]
     
     def __str__(self):
-        for car in self.cars:
-            return "\n".join(str(car) for car in self.cars)
+        return "\n".join(str(car) for car in self.cars)
 
     def add_cars_to_fleet(self,new_car):
         self.cars.append(new_car)
     
-    def remove_cars_from_flett(self,remove_car):
+    def remove_cars_from_fleet(self,remove_car):
         self.cars.remove(remove_car)
     
-    def sum_whole_fleet_milage(self):
-        return sum(car.milage for car in self.cars)
+    def sum_whole_fleet_mileage(self):
+        return sum(car.mileage for car in self.cars)
 
 def main():
     car1=Car("Toyota","Corolla",2023)
@@ -56,30 +60,30 @@ def main():
     try:
         car1.drive(1000000)
         car1.refuel(100000)
-    except FuelExcpetion as e:
+    except FuelException as e:
         print(e)
     #DivideByZero Catch
     try:
         car1.drive(0)
-    except ZeroDivisionError as e:
+    except ValueError as e:
         print(e)
     
     car2=Car("Mercedes","S",2023)
-    car2.drive(50)
-    car2.refuel(50)
+    car2.drive(500)
+    car2.refuel(20)
     
     car3=Car("Nissan","Skyline",1995)
     car3.drive(500)
-    car3.refuel(50)
+    car3.refuel(10000)
 
     fleet=Fleet()
     fleet.add_cars_to_fleet(car1)
     fleet.add_cars_to_fleet(car2)
     fleet.add_cars_to_fleet(car3)
     print(fleet)
-    fleet.remove_cars_from_flett(car1)
+    fleet.remove_cars_from_fleet(car1)
     print(fleet)
-    print(fleet.sum_whole_fleet_milage())
+    print(fleet.sum_whole_fleet_mileage())
     print(fleet)
 
 if __name__ == "__main__":
