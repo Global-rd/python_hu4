48c15e1a2ed90f7495d7cc978fe8339b0b8a59af
SandorAntal <tonisndr@outlook.com>
2026-05-17 20:51:40 +0200

    homeworks\sandorantal\hw_06_testing-modification

--- homeworks/sandorantal/hw_06_testing/test_bank_account.py
+++ homeworks/sandorantal/hw_06_testing/test_bank_account.py
@@ -3,11 +3,11 @@ from bank_account import BankAccount
 
 @pytest.fixture
 def empty_account():
-    return BankAccount(balance=0)
+    return BankAccount(owner="Spencer Charnas", balance=0)
 
 @pytest.fixture
 def wealthy_account():
-    return BankAccount(balance=1000)
+    return BankAccount(owner="Mark Hoppus", balance=1000)
 
 @pytest.mark.parametrize("amount", [0, -50, -100])
 def test_deposit_invalid_amounts(empty_account, amount):
