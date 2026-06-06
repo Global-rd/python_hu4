8cfd7f294a124d40f893f8a51c611fd699c3b2fe
4yPhyton <jakabm.man@gmail.com>
2026-06-06 17:03:04 +0100

    marcelljakab/homework-10

---
+++ homeworks/marcelljakab/hw_09_fastapi/services.py
@@ -0,0 +1,39 @@
+from fastapi import HTTPException
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from homeworks.marcelljakab.hw_09_fastapi.crud import (
+    create_item,
+    delete_item_by_id,
+    get_all_items,
+    get_item_by_id,
+    update_item_by_id,
+)
+from homeworks.marcelljakab.hw_09_fastapi.models import Item
+from homeworks.marcelljakab.hw_09_fastapi.schemas import ItemRequest
+
+
+async def list_items_service(db: AsyncSession) -> list[Item]:
+    return await get_all_items(db)
+
+
+async def get_item_service(item_id: str, db: AsyncSession) -> Item:
+    item = await get_item_by_id(item_id, db)
+    if item is None:
+        raise HTTPException(status_code=404, detail="Item id not found")
+    return item
+
+
+async def create_item_service(item: ItemRequest, db: AsyncSession) -> Item:
+    return await create_item(item, db)
+
+
+async def update_item_service(
+    item_id: str, item_update: ItemRequest, db: AsyncSession
+) -> Item:
+    item = await get_item_service(item_id, db)
+    return await update_item_by_id(item, item_update, db)
+
+
+async def delete_item_service(item_id: str, db: AsyncSession) -> Item:
+    item = await get_item_service(item_id, db)
+    return await delete_item_by_id(item, db)
